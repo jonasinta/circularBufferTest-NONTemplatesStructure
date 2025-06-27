aaab2f31504e4826c3430e9d6981992d1c08ca06
jonas <wompusi@gmail.com>
2025-06-27 18:20:12 +1200

    modified:   lib/TBuffer/CircularBuffer.h

--- lib/TBuffer/CircularBuffer.h
+++ lib/TBuffer/CircularBuffer.h
@@ -7,9 +7,12 @@ constexpr size_t BUFFER_SIZE = 10;
 
 // Declare buffer and related variables in RTC slow memory
 extern RTC_DATA_ATTR uint16_t EpochBuff[BUFFER_SIZE];
+<<<<<<< HEAD
+=======
 extern RTC_DATA_ATTR float Float1[BUFFER_SIZE];
 extern RTC_DATA_ATTR float Float2[BUFFER_SIZE];
 extern RTC_DATA_ATTR float Float3[BUFFER_SIZE];
+>>>>>>> 146e81f (started the coversion to composite buffer)
 extern RTC_DATA_ATTR size_t head;
 extern RTC_DATA_ATTR size_t tail;
 extern RTC_DATA_ATTR size_t itemCount;
